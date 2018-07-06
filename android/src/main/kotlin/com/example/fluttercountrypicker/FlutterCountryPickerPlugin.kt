45ee75bf418b49b7a1b9340d431f98808faf801f
Alessandro Biessek <alessandro@biessek.rocks>
2018-07-06 10:35:51 -0300

    added android code

--- android/src/main/kotlin/com/example/fluttercountrypicker/FlutterCountryPickerPlugin.kt
+++ android/src/main/kotlin/com/example/fluttercountrypicker/FlutterCountryPickerPlugin.kt
@@ -5,21 +5,34 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import java.util.*
+import kotlin.collections.HashMap
 
 class FlutterCountryPickerPlugin(): MethodCallHandler {
+  private val CHANNEL = "biessek.rocks/flutter_country_picker"
+  private val GET_COUNTRY_NAMES = "getCountryNames"
+
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar): Unit {
-      val channel = MethodChannel(registrar.messenger(), "flutter_country_picker")
+      val channel = MethodChannel(registrar.messenger(), CHANNEL)
       channel.setMethodCallHandler(FlutterCountryPickerPlugin())
     }
   }
 
   override fun onMethodCall(call: MethodCall, result: Result): Unit {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method.equals(GET_COUNTRY_NAMES))) {
+      result.success(getAllCountryNames(call.argument("isoCodes")))
     } else {
       result.notImplemented()
     }
+  }
+
+  private fun getAllCountryNames(isoCodes: ArrayList<String>): HashMap<String, String> {
+    val localCountries = HashMap<String, String>()
+    for (isoCode in isoCodes) {
+      localCountries[isoCode] = Locale("", isoCode).displayCountry
+    }
+    return localCountries
   }
 }
