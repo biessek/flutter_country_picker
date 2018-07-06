b7453beac47115618ed273962229e213f7eaeb71
Alessandro Biessek <alessandro@biessek.rocks>
2018-07-06 10:40:41 -0300

    added ios code

--- ios/Classes/SwiftFlutterCountryPickerPlugin.swift
+++ ios/Classes/SwiftFlutterCountryPickerPlugin.swift
@@ -3,12 +3,28 @@ import UIKit
     
 public class SwiftFlutterCountryPickerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_country_picker", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "biessek.rocks/flutter_country_picker", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterCountryPickerPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if("getCountryNames" == call.method) {
+        let args = call.arguments as! NSDictionary
+        self.receiveCountryNames(result: result, isoCodes: args.value(forKey: "isoCodes") as! [String])
+    } else {
+        result("MethodNotImplemented")
+    }
+  }
+
+  private func receiveCountryNames(result: FlutterResult, isoCodes: [String]) {
+    var countryNames: Dictionary<String, String> = Dictionary.init()
+    
+    let currentLocale: Locale = Locale.current
+    for code in isoCodes {
+        countryNames[code] = currentLocale.localizedString(forRegionCode: code)
+    }
+    
+    result(countryNames as NSDictionary)
   }
 }
