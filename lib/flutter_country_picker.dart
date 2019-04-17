8de6628d8e82325e9beac39ff59ded8e2adeb153
Alfred <alphy.addo@gmail.com>
2019-04-17 11:39:02 +0000

    Decide what to display- flag, dialing code, country name or all.

--- lib/flutter_country_picker.dart
+++ lib/flutter_country_picker.dart
@@ -38,11 +38,17 @@ class CountryPicker extends StatelessWidget {
     this.selectedCountry,
     @required this.onChanged,
     this.dense = false,
+    this.showName,
+    this.showDialingCode,
+    this.showFlag,
   }) : super(key: key);
 
   final Country selectedCountry;
   final ValueChanged<Country> onChanged;
   final bool dense;
+  final bool showName;
+  final bool showDialingCode;
+  final bool showFlag;
 
   @override
   Widget build(BuildContext context) {
@@ -65,13 +71,29 @@ class CountryPicker extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Image.asset(
-            displayCountry.asset,
-            package: "flutter_country_picker",
-            height: 48.0,
-            fit: BoxFit.fitWidth,
-          ),
-          Text(" (+ ${displayCountry.dialingCode})"),
+          Container(
+              child: showFlag
+                  ? Image.asset(
+                      displayCountry.asset,
+                      package: "flutter_country_picker",
+                      height: 32.0, //48.0 to 32.0
+                      fit: BoxFit.fitWidth,
+                    )
+                  : null),
+          Container(
+              child: showDialingCode
+                  ? Text(
+                      " (+${displayCountry.dialingCode})",
+                      style: TextStyle(fontSize: 20.0),
+                    )
+                  : null),
+          Container(
+              child: showName
+                  ? Text(
+                      " ${displayCountry.name}",
+                      style: TextStyle(fontSize: 22.0),
+                    )
+                  : null),
           Icon(Icons.arrow_drop_down,
               color: Theme.of(context).brightness == Brightness.light
                   ? Colors.grey.shade700
